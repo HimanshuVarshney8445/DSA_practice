2abe2ff2bf60298dddff77e274a2e52e71cee83e
HimanshuVarshney8445 <himanshu897946@gmail.com>
2026-07-09 07:06:24 +0530

    Time: 281 ms (5.05%), Space: 47.8 MB (77.19%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(list.contains(nums[i])){
+                list.remove(Integer.valueOf(nums[i]));
+            }else{
+                list.add(nums[i]);
+            }
+        }
+        int[] arr = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            arr[i]=list.get(i);
+        }
+        return arr;
+    }
+}
