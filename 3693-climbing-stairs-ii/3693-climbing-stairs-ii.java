b3601086cc6eeb3772b1a3b2e260daf18c7c2623
HimanshuVarshney8445 <himanshu897946@gmail.com>
2026-07-11 09:06:00 +0530

    Time: 8 ms (58.47%), Space: 117.1 MB (63.57%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
@@ -1,20 +1,17 @@
 class Solution {
-    public int find(int idx,int[] dp,int[] costs){
-        if(idx==0) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int min=Integer.MAX_VALUE;
-        for(int i=1;i<=3;i++){
-            int temp = idx-i;
-            if(temp>=0){
-                min=Math.min(min,find(temp,dp,costs)+costs[idx-1]+i*i);
-            }
-        }
-        dp[idx]=min;
-        return dp[idx];
-    }
     public int climbStairs(int n, int[] costs) {
         int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return find(n,dp,costs);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            int min=Integer.MAX_VALUE;
+            for(int j=1;j<=3;j++){
+                int temp = i-j;
+                if(temp>=0){
+                    min=Math.min(min,dp[temp]+costs[i-1]+j*j);
+                }
+            }
+            dp[i]=min;
+        }
+        return dp[n];
     }
 }
