64e2de2716269fe4696049ce0b47f4580b865696
HimanshuVarshney8445 <himanshu897946@gmail.com>
2026-07-30 22:23:41 +0530

    Time: 268 ms (25.86%), Space: 46.6 MB (16.8%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        int count=1;
+        StringBuilder sb = new StringBuilder(a);
+        if(sb.toString().contains(b)) return count;
+        while(sb.length()<b.length()){
+            sb.append(a);
+            count++;
+            if(sb.toString().contains(b)) return count;
+        }
+        sb.append(a);
+        count++;
+        if(sb.toString().contains(b)) return count;
+        return -1;
+    }
+}
